9da041db4bef8f83e19b1b811732d622bb2385bd
Simon Egersand <s.egersand@gmail.com>
2016-10-22 18:43:53 +0200

    Add dev server
    Add Gulp as task runner for development server and LESS processing.
    Gulp can be now be used for future tasks as well.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
+
+var paths = {
+    server: './index.js',
+    scripts: ['./*.js'],
+    templates: ['./views/*.pug'],
+    less: ['./views/*.less']
+};
+
+gulp.task('styles', function() {
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(livereload());
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(livereload());
+});
+
+gulp.task('templates', function() {
+    return gulp.src(paths.templates)
+        .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch(paths.less, ['styles']);
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.templates, ['templates']);
+});
+
+gulp.task('server', function() {
+    nodemon({
+        'script': paths.server
+    }).on('start', function() {
+        gulp.src(paths.server)
+            .pipe(livereload())
+    });
+});
+
+gulp.task('serve', ['server', 'watch']);
